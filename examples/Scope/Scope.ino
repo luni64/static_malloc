e7f84b9edcbb50c7aac3b1cf0ec54d2313623775
luni64 <lutz@niggl-schlagbauer.de>
2020-10-11 16:01:46 +0200

    Update Scope.ino

--- examples/Scope/Scope.ino
+++ examples/Scope/Scope.ino
@@ -2,8 +2,8 @@
 #include "oscilloscope.h"
 #include "static_malloc.h"
 
-constexpr size_t bufSize = 1024 * 200; // 100kB memory pool ...
-uint8_t myHeap[bufSize];        // ... on external RAM
+constexpr size_t bufSize = 1024 * 200; // 200kB memory pool ...
+EXTMEM uint8_t myHeap[bufSize];        // ... on external RAM
 
 //----------------------
 
@@ -15,8 +15,8 @@ void setup()
 {
     sm_set_default_pool(myHeap, bufSize, false, nullptr); // initialize memory pool
 
-    pinMode(stopPin, INPUT_PULLUP);                          // pin to stop measurement and print results
-    scope.begin(0, 1'000);                                   // scope on pin 0 with buffer for 1k transitions (class uses buffer from mem pool)
+    pinMode(stopPin, INPUT_PULLUP);                       // pin to stop measurement and print results
+    scope.begin(0, 1'000);                                // scope on pin 0 with buffer for 1k transitions (class uses buffer from mem pool)
 
     while (!Serial){}
     Serial.printf("Measuring... press button on pin %u to stop and print results\n", stopPin);
